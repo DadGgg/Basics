bcd7d715eb0ee67f1d896b29ab643238465f61eb
Scott <sgarland1962@yahoo.com>
2023-10-23 16:53:36 -0500

    Introduction: Types

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+const x: number = 2;
+const y: string = "Chad";
+let a = false;
+a = true;
+const isAlive: boolean = true;
+
+const result = addTwoNumbers(5, 8);
+const result2: string = addTwoNumbers(2, 3);
+
+function addTwoNumbers(a: number, b: number): string {
+    return `${a + b}`;
+}
